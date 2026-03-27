a3012f2177884214c6d5311a69e8864b7426c279
danmilithi <eudanielviana2@gmail.com>
2026-03-27 14:51:15 -0300

    feat: implementa pagina de catalogo com carrossel dinamico e sistema de trailers em JS

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Melhores avaliações",
+        items: [
+            {
+                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/embed/b9EkMc79ZSU" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/embed/HhesaQXLuRY" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/embed/KPLWWIOCOOQ" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/embed/uLtkt8BonwM" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "../assets/img-noctra.png", top10: true, youtube: "https://www.youtube.com/embed/TcMBFSGVi1c" },
+            { img: "../assets/img-noctra.png", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/embed/2AUmvWm5ZDQ" },
+            { img: "../assets/img-noctra.png", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/embed/zSWdZVtXT7E" },
+            { img: "../assets/img-noctra.png", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/embed/n9xhJrPXop4" },
+        ]
+    }
+];
